c29dc8ee60e73232cffe9408e00b755410a4ce3a
Winston Sevilla <wsevilla@blastasia.com>
2020-10-05 21:14:23 +0800

    Navigation Part 2

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,9 +9,12 @@ import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angul
 
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HomeComponent } from './home/home.component';
 
 import { DishService } from './services/dish.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 import { baseURL } from './shared/baseurl';
 
@@ -35,12 +38,15 @@ import { baseURL } from './shared/baseurl';
     declarations: [
         AppComponent,
         MenuComponent,
-        DishdetailComponent
+        DishdetailComponent,
+        HomeComponent
     ],
     providers: [
         {provide: 'baseURL', useValue: baseURL},
         DishService,
-        ProcessHTTPMsgService
+        ProcessHTTPMsgService,
+        PromotionService,
+        LeaderService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
