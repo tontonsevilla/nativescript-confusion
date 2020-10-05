5585b4f7ebd69e68bc38fdda99203385384d497f
Winston Sevilla <wsevilla@blastasia.com>
2020-10-05 20:19:42 +0800

    NativeScript Angular

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,15 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
 
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { HttpClientModule } from '@angular/common/http';
+import { NativeScriptHttpModule } from "nativescript-angular/http";
+
+import { MenuComponent } from './menu/menu.component';
+
+import { DishService } from './services/dish.service';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+
+import { baseURL } from './shared/baseurl';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -19,20 +25,24 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpModule,
+        HttpClientModule
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        MenuComponent
     ],
     providers: [
-        ItemService
+        {provide: 'baseURL', useValue: baseURL},
+        DishService,
+        ProcessHTTPMsgService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
 })
+
 /*
 Pass your application module to the bootstrapModule function located in main.ts to start your app
 */
