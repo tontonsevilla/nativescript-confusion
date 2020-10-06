a94a80bdcb8681b5b140d4f6b9164892c305e7d7
Winston Sevilla <wsevilla@blastasia.com>
2020-10-06 22:54:38 +0800

    Buttons

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -5,6 +5,8 @@ import { DishService } from '../services/dish.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RouterExtensions } from 'nativescript-angular/router';
 import { switchMap } from 'rxjs/operators';
+import { FavoriteService } from '~/services/favorite.service';
+import { Toasty } from 'nativescript-toasty';
 
 @Component({
   selector: 'app-dishdetail',
@@ -18,17 +20,41 @@ export class DishdetailComponent implements OnInit {
   comment: Comment;
   errMess: string;
 
-  constructor(private dishservice: DishService,
+  avgstars: string;
+  numcomments: number;
+  favorite: boolean = false;
+
+  constructor(
+    private dishservice: DishService,
     private route: ActivatedRoute,
     private routerExtensions: RouterExtensions,
-    @Inject('baseURL') private baseURL) { }
+    private favoriteservice: FavoriteService,
+    @Inject('baseURL') private baseURL
+  ) { }
 
   ngOnInit() {
 
     this.route.params
       .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => this.dish = dish,
-          errmess => { this.dish = null; this.errMess = <any>errmess; });
+      .subscribe(dish => { 
+          this.dish = dish;
+          this.favorite = this.favoriteservice.isFavorite(this.dish.id);
+          this.numcomments = this.dish.comments.length;
+
+          let total = 0;
+          this.dish.comments.forEach(comment => total += comment.rating);
+          this.avgstars = (total/this.numcomments).toFixed(2);
+        },
+        errmess => { this.dish = null; this.errMess = <any>errmess; });
+  }
+
+  addToFavorites() {
+    if (!this.favorite) {
+      console.log('Adding to Favorites', this.dish.id);
+      this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+      const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
+      toast.show();
+    }
   }
 
   goBack(): void {
