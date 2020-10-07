27c3e8d64a56d5e6ffecfb1a0f7b0c4f45b5e07b
Winston Sevilla <wsevilla@blastasia.com>
2020-10-07 15:33:29 +0800

    Forms

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,6 +8,8 @@ import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 import { TNSFontIconModule } from 'nativescript-ngx-fonticon';
 import { NativeScriptUIListViewModule } from "nativescript-ui-listview/angular";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
@@ -16,6 +18,7 @@ import { ContactComponent } from "./contact/contact.component";
 import { AboutComponent } from "./about/about.component";
 import { DrawerComponent } from "./shared/drawer/drawer.component";
 import { FavoritesComponent } from './favorites/favorites.component';
+import { ReservationComponent } from "./reservation/reservation.component";
 
 import { DishService } from './services/dish.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
@@ -45,7 +48,9 @@ import { baseURL } from './shared/baseurl';
         TNSFontIconModule.forRoot({
             'fa': './fonts/font-awesome.min.css'
         }),
-        NativeScriptUIListViewModule
+        NativeScriptUIListViewModule,
+        NativeScriptFormsModule,
+        ReactiveFormsModule
     ],
     declarations: [
         AppComponent,
@@ -55,7 +60,8 @@ import { baseURL } from './shared/baseurl';
         ContactComponent,
         AboutComponent,
         DrawerComponent,
-        FavoritesComponent
+        FavoritesComponent,
+        ReservationComponent
     ],
     providers: [
         {provide: 'baseURL', useValue: baseURL},
