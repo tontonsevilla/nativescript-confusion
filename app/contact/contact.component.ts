f7ca72cf49d73ec2b8e7422ce2656a33d52afa07
Winston Sevilla <wsevilla@blastasia.com>
2020-10-09 14:17:53 +0800

    Sending Email

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { DrawerPage } from '~/shared/drawer/drawer.page';
-
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import * as Email from 'nativescript-email';
 
 @Component({
   selector: 'app-contact',
@@ -13,12 +14,28 @@ export class ContactComponent extends DrawerPage {
 
   constructor(
     private changeDetectorRef: ChangeDetectorRef,
+    private fonticon: TNSFontIconService
   ) { 
     super(changeDetectorRef);
   }
 
   onDrawerButtonTap(): void {
     this.openDrawer();
-}
+  }
+
+  sendEmail() {
+    Email.available()
+      .then((avail: boolean) => {
+        if (avail) {
+          Email.compose({
+            to: ['confusion@food.net'],
+            subject: '[ConFusion]: Query',
+            body: 'Dear Sir/Madam:'
+          });
+        }
+        else
+          console.log('No Email Configured');
+      });
+  }
 
 }
