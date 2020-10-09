c011cf63a350f775711c9f5446d2e57f06fb918e
Winston Sevilla <wsevilla@blastasia.com>
2020-10-09 14:39:14 +0800

    Social Sharing

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -16,6 +16,8 @@ import { View } from "ui/core/view";
 import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
 import { Color } from 'color';
 import * as enums from "ui/enums";
+import * as SocialShare from "nativescript-social-share";
+import { ImageSource, fromUrl } from "image-source";
 
 @Component({
   selector: 'app-dishdetail',
@@ -94,12 +96,12 @@ export class DishdetailComponent implements OnInit {
     this.routerExtensions.back();
   }
 
-  showMoreAction(): void {
+  displayActionDialog(): void {
     dialogs
       .action({
         message: 'More Actions',
         cancelButtonText: 'Cancel',
-        actions: ['Add to Favorites', 'Add Comment'],
+        actions: ['Add to Favorites', 'Add Comment', 'Social Sharing'],
       })
       .then((result) => {
         console.log('Dialog result: ' + result);
@@ -128,6 +130,8 @@ export class DishdetailComponent implements OnInit {
                 this.setOtherDetails();
               }
             });
+        } else if (result === 'Social Sharing') {
+          this.socialShare();
         }
       });
   }
@@ -221,6 +225,18 @@ export class DishdetailComponent implements OnInit {
     .catch((e) => {
         console.log(e.message);
     });
-  } 
+  }
+
+  socialShare() {
+    let image: ImageSource;
+
+    fromUrl(this.baseURL + this.dish.image)
+     .then((img: ImageSource) => {
+       image = img; 
+        SocialShare.shareImage(image, "How would you like to share this image?")
+      })
+     .catch(()=> { console.log('Error loading image'); });
+
+  }
   
 }
