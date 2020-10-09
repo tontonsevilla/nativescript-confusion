1db1568346e1a6ce685338af415f8ec27cc33bfa
Winston Sevilla <wsevilla@blastasia.com>
2020-10-09 15:08:40 +0800

    Platform and Connectivity

--- app/app.module.ts
+++ app/app.module.ts
@@ -29,6 +29,7 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { FavoriteService } from "./services/favorite.service";
 import { CouchbaseService } from './services/couchbase.service';
+import { PlatformService } from './services/platform.service';
 
 import { baseURL } from './shared/baseurl';
 
@@ -80,7 +81,8 @@ import { baseURL } from './shared/baseurl';
         PromotionService,
         LeaderService,
         FavoriteService,
-        CouchbaseService
+        CouchbaseService,
+        PlatformService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
--- app/shared/drawer/drawer.component.ts
+++ app/shared/drawer/drawer.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PlatformService } from '~/services/platform.service';
 
 @Component({
     selector: 'drawer-content',
     templateUrl: './shared/drawer/drawer.component.html'
 })
-export class DrawerComponent {
+export class DrawerComponent implements OnInit, OnDestroy {
 
-    constructor() {
+    constructor(
+        private platformService: PlatformService
+    ) {
         
+    }
+
+    ngOnInit(): void {
+        this.platformService.printPlatformInfo(); 
+        this.platformService.startMonitoringNetwork()
+        .subscribe((message: string) => {
+        console.log(message); 
+        
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.platformService.stopMonitoringNetwork();
     }
 
 }
